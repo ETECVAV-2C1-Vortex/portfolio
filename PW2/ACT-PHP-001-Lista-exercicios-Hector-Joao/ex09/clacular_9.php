4b3fa3eeaa4d84b7c059079b3005aa13f2230e00
hectorelias04 <hectorelias11100@gmail.com>
2026-03-29 20:52:50 -0300

    php9

---
+++ PW2/ACT-PHP-001-Lista-exercicios-Hector-Joao/ex09/clacular_9.php
@@ -0,0 +1,32 @@
+<!-- 
+ Autor do codigo: Hector Elias
+ sala : 2C¹
+ exercicio 9 
+ lista 1 
+-->
+
+<?php
+function media($v){
+    $soma = 0;
+    $quantidade = count($v);
+    foreach ($v as $valor){
+        $soma += $valor;
+    }
+    if ($quanidade > 0) {
+        return $soma / $quantidade;
+    } else {
+        return 0;
+    }
+}
+  if (isset($_GET['numeros'])){
+    $entrada = $_GET['numeros'];
+    $numeros = explode(",", $entrada);
+    $numeros = array_map('floatval', $numeros);
+    $resultado = media($numeros);
+    echo "Resultado da Média: $resultado";
+   } else {
+        echo "Nenhum valor encontrado";
+    }
+  
+?>
+
---
+++ PW2/ACT-PHP-001-Lista-exercicios-Hector-Joao/ex09/index_9.php
@@ -0,0 +1,27 @@
+<!-- 
+ Autor do codigo: Hector Elias
+ sala : 2C¹
+ exercicio 9 
+ lista 1 
+-->
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <h1>Média aritimética </h1>
+    </head>
+<body>
+
+    <h2>Calcular Média Aritmética</h2>
+
+    <form action="clacular_9.php" method="GET">
+        <label>Digite números separados por vírgula:</label><br><br>
+        <input type="text" name="numeros"><br>
+        <button type="submit">Calcular</button>
+    </form>
+
+</body>
+</html>
