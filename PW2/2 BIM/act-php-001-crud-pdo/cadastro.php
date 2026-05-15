ac4b0a7fdf699718aecad741f0fce13dee5a1e4d
Viola2365 <igortkmatheus@gmail.com>
2026-05-15 08:53:51 -0300

    Início do desenvolvimento de cadastro.php

---
+++ PW2/2 BIM/act-php-001-crud-pdo/cadastro.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cadastro</title>
+</head>
+<body>
+
+<h2>Cadastrar aluno</h2>
+
+<form action="cadastro.php" method="POST">
+
+    <label>Nome:</label>
+    <input type="text" name="nome" required>
+    <br><br>
+
+    <label>Fabricante</label>
+    <input type="text" name="fabricante" required>
+    <br><br>
+
+    <label>Preço:</label>
+    <input type="number" step="0.01" name="preco" required>
+    <br><br>
+
+    <label>Estoque:</label>
+    <input type="number" name="estoque" required>
+    <br><br>
+
+
+    <button type="submit">Salvar</button>
+
+</form>
+
+</body>
+</html>
+<?php
+require "config/conexao.php";
+
+$nome = $_POST['nome'];
+
+$fabricante = $_POST['fabricante'];
+
+$preco = $_POST['preco'];
+
+$estoque = $_POST['estoque'];
+
+$sql = "INSERT INTO produtos (nome, fabricante, preco, estoque)
+        VALUES (:nome, :fabricante, :preco, :estoque)";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([
+    ':nome' => $nome,
+    ':fabricante' => $fabricante,
+    ':preco' => $preco,
+    ':estoque' => $estoque
+]);
+
+
+?>
