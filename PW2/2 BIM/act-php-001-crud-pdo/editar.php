1202a971fa0634cd8ea31d0bc2bc25fd31e16049
IgorEustachio <igor.daniel.eustachio@gmail.com>
2026-05-19 18:02:04 -0300

    lógica de edição concluída

--- PW2/2 BIM/act-php-001-crud-pdo/editar.php
+++ PW2/2 BIM/act-php-001-crud-pdo/editar.php
@@ -1,19 +1,20 @@
-
-
 <?php
-require 'includes/header.php';
-
 require_once "config/conexao.php";
+require 'includes/header.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $id             = 2;
-    $novoNome       = $_POST['nome'];
-    $novoFabricante = $_POST['fabricante'];
-    $novoPreco      = $_POST['preco'];
-    $novoEstoque    = $_POST['estoque'];
+    $id = $_POST['id'];
 
-    $sql = "UPDATE produtos SET nome = :novo_nome, fabricante = :novo_fabricante, 
+    $novoNome = $_POST['nome'];
+
+    $novoFabricante = $_POST['fabricante'];
+
+    $novoPreco = $_POST['preco'];
+
+    $novoEstoque = $_POST['estoque'];
+
+   $sql = "UPDATE produtos SET nome = :novo_nome, fabricante = :novo_fabricante, 
             preco = :novo_preco, estoque = :novo_estoque WHERE id = :id";
     $stmt = $pdo->prepare($sql);
     $sucesso = $stmt->execute([
@@ -28,44 +29,65 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header("Location: index.php");
         exit;
     }
+} elseif (isset($_GET['id'])) {
+    $id = $_GET['id'];
 
-} else {
+    $sql = "SELECT * FROM produtos WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
 
-    $id = 2;
-    $stmt = $pdo->prepare("SELECT * FROM produtos WHERE id = :id");
+    // 3. EXECUTAR: Passamos o valor real para o apelido :id
     $stmt->execute([':id' => $id]);
-    $produtos = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // 4. BUSCAR: Como o ID é único, usamos fetch() em vez de fetchAll()
+    // O FETCH_ASSOC serve para que o resultado venha como um array ['campo' => 'valor']
+    $produto = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      if ($produto === false) {
+        header("Location: editar.php?erro=ID não encontrado");
+        exit;
+  }
+} else {
 
 }
 ?>
 
+  <?php if (!isset($produto)): ?>
+     <form action="editar.php" method="GET">
+        <label>ID do produto:</label>
+        <input type="number" name="id" required>
+        <br><br>
+        <button type="submit">Buscar</button>
+     </form>
+  <?php elseif (isset($produto)): ?>
+     <form action="editar.php" method="POST">
 
-<h2>Editar produto</h2>
-
-<form action="editar.php" method="POST">
-
-    <input type="hidden" name="id" value="<?php echo $produtos['id']; ?>">
+    <input type="hidden" name="id" value="<?php echo $produto['id']; ?>">
 
     <label>Nome do Produto:</label>
-    <input type="text" name="nome" value="<?php echo $produtos['nome']; ?>" required>
+    <input type="text" name="nome" value="<?php echo $produto['nome']; ?>" required>
     <br><br>
 
     <label>Fabricante:</label>
-    <input type="text" name="fabricante" value="<?php echo $produtos['fabricante']; ?>" required>
+    <input type="text" name="fabricante" value="<?php echo $produto['fabricante']; ?>" required>
     <br><br>
 
     <label>Preço (R$):</label>
-    <input type="number" step="0.01" name="preco" value="<?php echo $produtos['preco']; ?>" required>
+    <input type="number" step="0.01" name="preco" value="<?php echo $produto['preco']; ?>" required>
     <br><br>
 
     <label>Estoque Atual:</label>
-    <input type="number" name="estoque" value="<?php echo $produtos['estoque']; ?>" required>
+    <input type="number" name="estoque" value="<?php echo $produto['estoque']; ?>" required>
     <br><br>
 
     <button type="submit">Salvar</button>
 
 </form>
+  <?php endif; ?>
 
-<?php
-require 'includes/footer.php'
+    <?php if (isset($_GET['erro'])): ?>
+      <p><?php echo $_GET['erro']; ?></p>
+  <?php endif; ?>
+
+<?php 
+require 'includes/footer.php';
 ?>
